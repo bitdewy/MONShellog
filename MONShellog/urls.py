70652bb4a4ef80d151fb768767cc3f3a63ef5d49
bitdewy <bitdewy@gmail.com>
2012-06-21 00:53:58 +0800

    fix static file serve bug

--- MONShellog/urls.py
+++ MONShellog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from MONShellog.blog.feeds import *
 from django.conf import settings
-from django.conf.urls.static import static
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -24,15 +24,15 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    (r'^admin/(.*)', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     (r'^$', 'MONShellog.blog.views.index'),
     (r'^archive/(?P<year>\d{4})/(?P<month>(\d{1,2})?)/$', 'MONShellog.blog.views.month'),
     (r'^archives/$', 'MONShellog.blog.views.archive'),
     (r'^links/$', 'MONShellog.blog.views.link'),
     (r'^entries/$', 'MONShellog.blog.views.entries'),
     (r'^comments/$', 'MONShellog.blog.views.comments'),
-    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed',{'feed_dict': feeds}),
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.Feed',{'feed_dict': feeds}),
     url(r'^entry/(?P<key>.*)/$', 'MONShellog.blog.views.entry',name = 'entry'),
     url(r'^category/(?P<cat>.*)/$', 'MONShellog.blog.views.category',name = 'cate'),
     url(r'^tag/(?P<tag>.*)/$', 'MONShellog.blog.views.tag',name = 'tag'),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
