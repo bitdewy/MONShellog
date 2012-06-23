22f67abd16915394814061d79757139f72148b46
bitdewy <bitdewy@gmail.com>
2012-06-23 23:03:31 +0800

    fix js bug

--- MONShellog/static/js/admin.textarea.js
+++ MONShellog/static/js/admin.textarea.js
@@ -1,10 +1,13 @@
 window.onload = function()
 {
     // create the FCK Editor and replace the specific text area with it
-    var oFCKeditor = new FCKeditor( 'id_body' ) ;
-    oFCKeditor.Width = '60%';
-    oFCKeditor.Height = '500';
-    oFCKeditor.BasePath	= '/media/fckeditor/';
-    oFCKeditor.ReplaceTextarea() ;
+    if (document.getElementById('id_body') != null)
+    {
+      var oFCKeditor = new FCKeditor('id_body') ;
+      oFCKeditor.Width = '60%';
+      oFCKeditor.Height = '500';
+      oFCKeditor.BasePath = '/media/fckeditor/';
+      oFCKeditor.ReplaceTextarea();
+    }
 }
 
